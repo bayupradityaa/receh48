2253da31419f193af48194e82ce96bd851509419
Bayu Praditya <132676040+bayupradityaa@users.noreply.github.com>
2024-09-14 12:32:20 +0700

    edit.js

--- edit.js
+++ edit.js
@@ -1,16 +1,30 @@
 function toggleMenu() {
-  const nav = document.getElementById('nav-menu');
   const menuToggle = document.querySelector('.menu-toggle');
-  nav.classList.toggle('active');
+  const nav = document.querySelector('nav');
+  const body = document.querySelector('body');
   menuToggle.classList.toggle('active');
+  nav.classList.toggle('active');
+  
+  if (menuToggle.classList.contains('active')) {
+      menuToggle.innerHTML = '&times;'; // Cross icon
+  } else {
+      menuToggle.innerHTML = '&#9776;'; // Burger icon
+  }
 }
 
-function closeMenu() {
-  const nav = document.getElementById('nav-menu');
-  const menuToggle = document.querySelector('.menu-toggle');
-  nav.classList.remove('active');
-  menuToggle.classList.remove('active');
-}
+let navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+      const nav = document.querySelector('nav');
+      const menuToggle = document.querySelector('.menu-toggle');
+      const body = document.querySelector('body');
+      
+      nav.classList.remove('active');
+      menuToggle.classList.remove('active');
+      body.classList.remove('blur-bg');
+      menuToggle.innerHTML = '&#9776;'; 
+  });
+});
 
 // form pemesanan
 const scriptURL = 'https://script.google.com/macros/s/AKfycby3sA8zkYrn-15VNn7j0HZNlaCs8X1Z0CnojLPEED2SLMHIMu4ctQXwk4WesKQkludpCw/exec';
@@ -23,7 +37,9 @@ let formIsOpen = false;  // Set ke 'false' untuk menutup form
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  
+
+  form.reset();
+
   // Periksa apakah form sedang terbuka
   if (!formIsOpen) {
     Swal.fire({
@@ -36,15 +52,20 @@ form.addEventListener('submit', e => {
   }
 });
 
-// animasi password
-const passwordField = document.getElementById('password');
-const togglePassword = document.getElementById('togglePassword');
+// eye password
+let eyeicon = document.getElementById("eyeicon");
+let password = document.getElementById("password");
 
-togglePassword.addEventListener('click', function() {
-  const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
-  passwordField.setAttribute('type', type);
-  this.textContent = type === 'password' ? '👁️' : '😎';
-});
+eyeicon.onclick = function () {
+  if (password.type == "password") {
+    password.type = "text";
+    eyeicon.src = "eye-open.png";
+  } else {
+    password.type = "password";
+    eyeicon.src = "eye-close.png";
+  }
+}
+
 
 // Animasi Masuk Section Home/Beranda
 ScrollReveal().reveal('.home-content h1', { delay: 400, origin: 'top', distance: '100px' });
